af425d193b7ab36d9379ac7980750726b7a74cb7
Kyle Hotchkiss <108172+kylehotchkiss@users.noreply.github.com>
2021-04-28 14:44:44 -0700

    signout functionality

--- website/src/components/Header.js
+++ website/src/components/Header.js
@@ -1,18 +1,38 @@
+import AuthContext from 'components/Auth/contexts/AuthContext';
+import { signOut } from 'firebase/auth';
+import firebase from 'lib/firebase';
 import Link from 'next/link';
+import { useContext } from 'react';
 import styles from './Header.module.scss';
 
 export default function Header() {
+  const auth = useContext(AuthContext);
+
+  console.log('auth', auth);
+
+  function logout(event) {
+    event.preventDefault();
+    signOut(firebase.auth);
+  }
+
   return (
     <header className={styles.header}>
       <div className="container">
-        <div className="row ">
-          <div className="col">
+        <div className="row">
+          <div className="col-8">
             <div className={styles.logo}>
               <Link href="/">
                 <h1>Site Title</h1>
               </Link>
             </div>
           </div>
+          {auth.loggedIn && (
+            <div className="col-4 text-end">
+              <a href="#" onClick={logout}>
+                Sign Out
+              </a>
+            </div>
+          )}
         </div>
       </div>
     </header>
